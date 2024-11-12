73c1869308531b6370234c0c5b5930576d115218
ZanottiMatteo <m.zanotti13@studenti.unibg.it>
2024-11-12 18:56:47 +0100

    Creazione Maven Project

---
+++ Java/BGTransport/src/main/java/BGTransport/BGTransport/App.java
@@ -0,0 +1,10 @@
+package BGTransport.BGTransport;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("BGTransport!");
+    }
+}
