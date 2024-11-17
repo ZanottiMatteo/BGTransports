4f2d201960eea208b0a7c43f13f926322f87625e
Riccardo Maffeis <r.maffeis4@studenti.unibg.it>
2024-11-17 18:52:02 +0100

    GenerationCode class

---
+++ Java/BGTransport/src/main/java/databases/GenerateCode.java
@@ -0,0 +1,24 @@
+package databases;
+
+import org.jooq.codegen.GenerationTool;
+import org.jooq.meta.jaxb.Configuration;
+import org.jooq.meta.jaxb.Database;
+import org.jooq.meta.jaxb.Generator;
+import org.jooq.meta.jaxb.Jdbc;
+import org.jooq.meta.jaxb.Target;
+
+// codice per generare i sorgenti a partire dal DB
+public class GenerateCode {
+
+    public static void main(String[] args) throws Exception {
+
+	Jdbc JDBC = new Jdbc().withDriver("org.sqlite.JDBC").withUrl(utility.Constant.DB_URL_PUBLIC_TRANSPORTATION);
+	Database database = new Database().withName("org.jooq.meta.sqlite.SQLiteDatabase").withIncludes(".*")
+		.withExcludes("");
+	Target target = new Target().withPackageName("contatti.jooq.generated").withDirectory("src-generated/");
+	Generator generator = new Generator().withDatabase(database).withTarget(target);
+	// generator.getGenerate().setPojos(true);
+	Configuration configuration = new Configuration().withJdbc(JDBC).withGenerator(generator);
+	GenerationTool.generate(configuration);
+    }
+}
