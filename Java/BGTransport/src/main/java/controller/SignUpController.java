3378692b29a2c80a4bce1fcff13ef735ba3017ba
Riccardo Maffeis <r.maffeis4@studenti.unibg.it>
2024-11-20 22:37:58 +0100

    completamento logica di inserimento dati nel database user

--- Java/BGTransport/src/main/java/controller/SignUpController.java
+++ Java/BGTransport/src/main/java/controller/SignUpController.java
@@ -37,56 +37,31 @@ public class SignUpController {
 		System.out.println( "Name: " + name + "\nsurname: " + surname + "\nbirthday: " + birthday + "\nEmail: " + email + "\nPassword: " + password +
 							"\npasswordconf: " + passwordconf + "\nusername: " + username + "\naddress: " + address + "\ncity: " + city + "\nzipcode: " + zipcode);
 		
-		if (name != null && surname != null && birthday != null && (birthday.contains("/") || birthday.contains("-"))) {
+		if (name != null && surname != null && birthday != null && (birthday.contains("/") || birthday.contains("-")) 
+		        && email != null && email.contains("@") && email.contains(".") 
+		        && password != null && passwordconf != null && password.equals(passwordconf) 
+		        && username != null && !username.contains(" ")) {
+			
 			MainController.users.add(newUser = new RegisteredUser(name, surname, birthday, role));
+			newUser.setEmail(email);
+			newUser.setPassword(password);
+			newUser.setUsername(username);
+			newUser.setAddress(address);
+			newUser.setCity(city);
+			newUser.setZIPcode(zipcode);
+			
 			user.insertInto(User.USER)
 			.set(User.USER.NAME, name)
 			.set(User.USER.SURNAME, surname)
 			.set(User.USER.DATE_OF_BIRTH, birthday)
 			.set(User.USER.ROLE, role)
-			.execute();
-		}
-		
-		if (email != null && email.contains("@") && email.contains(".")) {
-			newUser.setEmail(email);
-			user.insertInto(User.USER)
 			.set(User.USER.MAIL, email)
-			.execute();
-		}
-		
-		if ((password != null && passwordconf != null) && (password == passwordconf)) {
-			newUser.setPassword(password);
-			user.insertInto(User.USER)
 			.set(User.USER.PASSWORD, password)
-			.execute();
-		}
-		
-		if (username != null && !username.contains(" ")) {
-			newUser.setUsername(username);
-			user.insertInto(User.USER)
 			.set(User.USER.USERNAME, username)
-			.execute();
-		}
-		
-		if (address != null) {
-			newUser.setAddress(address);
-			user.insertInto(User.USER)
 			.set(User.USER.ADDRESS, address)
-			.execute();
-		}
-		
-		if (city != null) {
-			newUser.setCity(city);
-			user.insertInto(User.USER)
 			.set(User.USER.TOWN, city)
-			.execute();
-		}
-		
-		if (zipcode != null) {
-			newUser.setZIPcode(zipcode);
-			user.insertInto(User.USER)
 			.set(User.USER.CAP, zipcode)
 			.execute();
-		}		
+		}
 	}
 }
