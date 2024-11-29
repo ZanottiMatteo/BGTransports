4dc9b3b25ed2b4ef7f5fef1d5e85cb920a642352
Riccardo Maffeis <r.maffeis4@studenti.unibg.it>
2024-11-29 10:21:41 +0100

    aggiunta controlli sui database

--- Java/BGTransport/src/main/java/model/ControlDB.java
+++ Java/BGTransport/src/main/java/model/ControlDB.java
@@ -4,19 +4,41 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class ControlDB {
 
-	public static Boolean control(String json) {
+	public static void control(String json, String excel) throws Exception {
 		File jsonFile = new File(json);
 		if (!jsonFile.exists()) {
-			return false;
+			System.out.println("Creazione file JSON: " + json);
+			List<Map<String, String>> list = CreateJSONFile.readExcelFile(excel);
+			CreateJSONFile.writeJsonToFile(list, json);
 		}
-		return true;
+		else if(jsonFile.exists()) {
+			System.out.println("Controllo se il file JSON " + json + " e' aggiornato");
+			List<Map<String, String>> excelData = CreateJSONFile.readExcelFile(excel);
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> jsonData = new HashMap<>();
+            objectMapper.readValue(jsonFile, Map.class);
+            
+            if (!excelData.equals(jsonData)) {
+                // 4. Aggiorna il JSON se i dati sono cambiati
+                objectMapper.writeValue(jsonFile, excelData);
+                System.out.println("File JSON aggiornato con i dati dall'Excel.");
+            } else {
+                System.out.println("Il file JSON è già aggiornato.");
+            }
+		}
 	}
 	
 	public static DSLContext DSLContext(String database) throws SQLException {
--- Java/BGTransport/src/main/java/model/MainModel.java
+++ Java/BGTransport/src/main/java/model/MainModel.java
@@ -17,61 +17,14 @@ public class MainModel {
 
 		GenerateCode.generateCode(Constant.DB_URL_PUBLIC_TRANSPORTATION, Constant.JDBC, Constant.SQLiteDatabase,Constant.JOOQ_TRANSPORTATION, Constant.SRC_TRANSPORTATION);
 
-		Boolean controlFunicularStation = ControlDB.control(Constant.JSON_FUNICULAR_STATION);
-		if (controlFunicularStation == false) {
-			System.out.println("start funicular station");
-			List<Map<String, String>> funicularStation = CreateJSONFile.readExcelFile(Constant.EXCEL_FUNICULAR_STATION_LIST);
-			CreateJSONFile.writeJsonToFile(funicularStation, Constant.JSON_FUNICULAR_STATION);
-		}
-
-		Boolean controlCompany = ControlDB.control(Constant.JSON_COMPANY);
-		if (controlCompany == false) {
-			System.out.println("start company");
-			List<Map<String, String>> companies = CreateJSONFile.readExcelFile(Constant.EXCEL_COMPANIES_LIST);
-			CreateJSONFile.writeJsonToFile(companies, Constant.JSON_COMPANY);
-		}
-
-		Boolean controlPullmanStop = ControlDB.control(Constant.JSON_PULLMAN_STOP);
-		if (controlPullmanStop == false) {
-			System.out.println("start pullman stop");
-			List<Map<String, String>> pullmanStop = CreateJSONFile.readExcelFile(Constant.EXCEL_PULLMAN_STOP_LIST);
-			CreateJSONFile.writeJsonToFile(pullmanStop, Constant.JSON_PULLMAN_STOP);
-		}
-
-		Boolean controlTrainStation = ControlDB.control(Constant.JSON_TRAIN_STATION);
-		if (controlTrainStation == false) {
-			System.out.println("start train station");
-			List<Map<String, String>> trainStation = CreateJSONFile.readExcelFile(Constant.EXCEL_TRAIN_STATION_LIST);
-			CreateJSONFile.writeJsonToFile(trainStation, Constant.JSON_TRAIN_STATION);
-		}
-
-		Boolean controlTramStop = ControlDB.control(Constant.JSON_TRAM_STOP);
-		if (controlTramStop == false) {
-			System.out.println("start tram stop");
-			List<Map<String, String>> tramStop = CreateJSONFile.readExcelFile(Constant.EXCEL_TRAM_STOP_LIST);
-			CreateJSONFile.writeJsonToFile(tramStop, Constant.JSON_TRAM_STOP);
-		}
-
-		Boolean controlFunicularTimetbale = ControlDB.control(Constant.JSON_FUNICULAR_TIMETABLE);
-		if (controlFunicularTimetbale == false) {
-			System.out.println("start funicular timetable");
-			List<Map<String, String>> funicularTimetable = CreateJSONFile.readExcelFile(Constant.EXCEL_FUNICULAR_TIMETABLE);
-			CreateJSONFile.writeJsonToFile(funicularTimetable, Constant.JSON_FUNICULAR_TIMETABLE);
-		}
-
-		Boolean controlTramTimetable = ControlDB.control(Constant.JSON_TRAM_TIMETABLE);
-		if (controlTramTimetable == false) {
-			System.out.println("start tram timetable");
-			List<Map<String, String>> tramTimetable = CreateJSONFile.readExcelFile(Constant.EXCEL_TRAM_TIMETABLE);
-			CreateJSONFile.writeJsonToFile(tramTimetable, Constant.JSON_TRAM_TIMETABLE);
-		}
-		
-		Boolean controlTrainTimetable = ControlDB.control(Constant.JSON_TRAIN_TIMETABLE);
-		if (controlTrainTimetable == false) {
-			System.out.println("start train timetable");
-			List<Map<String, String>> trainTimetable = CreateJSONFile.readExcelFile(Constant.EXCEL_TRAIN_TIMETABLE);
-			CreateJSONFile.writeJsonToFile(trainTimetable, Constant.JSON_TRAIN_TIMETABLE);
-		}
+		ControlDB.control(Constant.JSON_FUNICULAR_STATION, Constant.EXCEL_FUNICULAR_STATION_LIST);
+		ControlDB.control(Constant.JSON_COMPANY, Constant.EXCEL_COMPANIES_LIST);
+		ControlDB.control(Constant.JSON_PULLMAN_STOP, Constant.EXCEL_PULLMAN_STOP_LIST);
+		ControlDB.control(Constant.JSON_TRAIN_STATION, Constant.EXCEL_TRAIN_STATION_LIST);
+		ControlDB.control(Constant.JSON_TRAM_STOP, Constant.EXCEL_TRAM_STOP_LIST);
+		ControlDB.control(Constant.JSON_FUNICULAR_TIMETABLE, Constant.EXCEL_FUNICULAR_TIMETABLE);
+		ControlDB.control(Constant.JSON_TRAM_TIMETABLE, Constant.EXCEL_TRAM_TIMETABLE);
+		ControlDB.control(Constant.JSON_TRAIN_TIMETABLE, Constant.EXCEL_TRAIN_TIMETABLE);
 
 		InsertDataDB.company(createPublicTrasnportation);
 		InsertDataDB.funicular_station(createPublicTrasnportation);
