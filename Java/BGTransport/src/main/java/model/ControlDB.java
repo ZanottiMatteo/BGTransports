eac34f48226e05eff6fbf3ac05ef08d6926f2a03
Riccardo Maffeis <r.maffeis4@studenti.unibg.it>
2024-12-04 11:33:36 +0100

    sistemazione ControlDB

--- Java/BGTransport/src/main/java/model/ControlDB.java
+++ Java/BGTransport/src/main/java/model/ControlDB.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,11 +18,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class ControlDB {
 	public static void controlJSON(String json, String excel) throws Exception {
 		File jsonFile = new File(json);
+		ArrayList<String> myList = new ArrayList<>();
 		if (!jsonFile.exists()) {
 			System.out.println("File JSON creation: " + json);
 			List<Map<String, String>> list = CreateJSONFile.readExcelFile(excel);
 			CreateJSONFile.writeJsonToFile(list, json);
 			Constant.update = true;
+			myList.add(json);
 		} else {
 			System.out.println("Check if file JSON is up-to-date: " + json);
 			List<Map<String, String>> excelData = CreateJSONFile.readExcelFile(excel);
@@ -47,46 +50,60 @@ public class ControlDB {
 		if (!dbFile.exists()) {
 			System.out.println("Database creation: " + database);
 			CreateDB.createDatabase(database);
-			
+
 			System.out.println("Creating tables in the database: " + database + " ...");
-			DSLContext createPublicTrasnportation = ControlDB.DSLContext(database);
-			CreateTablesDB.createTablesPublicTransportation(createPublicTrasnportation);
+			DSLContext create = ControlDB.DSLContext(database);
+			
+			if (databasePath == Constant.DB_PUBLIC_TRANSPORTATION) {
+				CreateTablesDB.createTablesPublicTransportation(create);
+			} else {
+				CreateTablesDB.createTablesUsers(create);
+			}
+
 			GenerateCode.generateCode(database, Constant.JDBC, Constant.SQLiteDatabase, jooq, src);
-			
-			InsertDataDB.company(createPublicTrasnportation);
-			InsertDataDB.funicular_station(createPublicTrasnportation);
-			InsertDataDB.train_station(createPublicTrasnportation);
-			InsertDataDB.tram_stop(createPublicTrasnportation);
-			InsertDataDB.pullman_stop(createPublicTrasnportation);
-			InsertDataDB.funicularTimetable(createPublicTrasnportation);
-			InsertDataDB.tramTimetable(createPublicTrasnportation);
-			InsertDataDB.trainTimetable(createPublicTrasnportation);
-			//InsertDataDB.pullmanTimetable(createPublicTrasnportation);
-			
+
+			if (databasePath == Constant.DB_PUBLIC_TRANSPORTATION) {
+				InsertDataDB.company(create);
+				InsertDataDB.funicular_station(create);
+				InsertDataDB.train_station(create);
+				InsertDataDB.tram_stop(create);
+				InsertDataDB.pullman_stop(create);
+				InsertDataDB.funicularTimetable(create);
+				InsertDataDB.tramTimetable(create);
+				InsertDataDB.trainTimetable(create);
+				InsertDataDB.pullmanTimetable(create);
+			}
+
 		} else if (Constant.update) {
 			System.out.println("Database update: " + database);
-			dbFile.delete();
-			CreateDB.createDatabase(database);
-			
-			System.out.println("Creating tables in the database: " + database + " ...");
-			DSLContext createPublicTrasnportation = ControlDB.DSLContext(database);
-			CreateTablesDB.createTablesPublicTransportation(createPublicTrasnportation);
-			GenerateCode.generateCode(database, Constant.JDBC, Constant.SQLiteDatabase, jooq, src);
-			
-			InsertDataDB.company(createPublicTrasnportation);
-			InsertDataDB.funicular_station(createPublicTrasnportation);
-			InsertDataDB.train_station(createPublicTrasnportation);
-			InsertDataDB.tram_stop(createPublicTrasnportation);
-			InsertDataDB.pullman_stop(createPublicTrasnportation);
-			InsertDataDB.funicularTimetable(createPublicTrasnportation);
-			InsertDataDB.tramTimetable(createPublicTrasnportation);
-			InsertDataDB.trainTimetable(createPublicTrasnportation);
-			//InsertDataDB.pullmanTimetable(createPublicTrasnportation);
-			
+			// dbFile.delete();
+			// CreateDB.createDatabase(database);
+
+			// System.out.println("Creating tables in the database: " + database + " ...");
+			// DSLContext createPublicTrasnportation = ControlDB.DSLContext(database);
+			// CreateTablesDB.createTablesPublicTransportation(createPublicTrasnportation);
+			// GenerateCode.generateCode(database, Constant.JDBC, Constant.SQLiteDatabase,
+			// jooq, src);
+
+			// InsertDataDB.company(createPublicTrasnportation);
+			// InsertDataDB.funicular_station(createPublicTrasnportation);
+			// InsertDataDB.train_station(createPublicTrasnportation);
+			// InsertDataDB.tram_stop(createPublicTrasnportation);
+			// InsertDataDB.pullman_stop(createPublicTrasnportation);
+			// InsertDataDB.funicularTimetable(createPublicTrasnportation);
+			// InsertDataDB.tramTimetable(createPublicTrasnportation);
+			// InsertDataDB.trainTimetable(createPublicTrasnportation);
+			// InsertDataDB.pullmanTimetable(createPublicTrasnportation);
+
 			Constant.update = false;
 		} else {
 			System.out.println("Database already existed and is up-to-date: " + database);
 		}
+	}
+
+	public static void deleteAll(DSLContext ctx) {
+		// ctx.deleteFrom(TABLE_NAME)
+		// .execute();
 	}
 
 	public static DSLContext DSLContext(String database) throws SQLException {
